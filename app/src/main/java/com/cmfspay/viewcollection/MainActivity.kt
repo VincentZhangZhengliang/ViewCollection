5631236283107fa2b0537536078b41b823318441
Joker <529889220@qq.com>
2018-09-30 16:41:45 +0800

    ssss

--- app/src/main/java/com/cmfspay/viewcollection/MainActivity.kt
+++ app/src/main/java/com/cmfspay/viewcollection/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.cmfspay.viewcollection
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.cmfspay.viewcollection.foundation.FoundationActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initListener()
     }
+
+    private fun initListener() {
+        btn_view_foundation.setOnClickListener { startActivity(Intent(this, FoundationActivity::class.java)) }
+    }
+
 }
---
+++ app/src/main/java/com/cmfspay/viewcollection/foundation/FoundationActivity.kt
@@ -0,0 +1,14 @@
+package com.cmfspay.viewcollection.foundation
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.cmfspay.viewcollection.R
+
+class FoundationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_foundation)
+    }
+
+}
